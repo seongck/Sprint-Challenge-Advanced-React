dca9cafc63350083c2627555a7b109c482bb1c3f
seongck <seongk@pm.me>
2019-09-13 19:40:35 -0700

    Add darkmode styles and functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 function Navbar() {
+
+  const [ darkMode, setDarkMode ] = useDarkMode(false);
+
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  }
+
   return (
     <nav className="navbar">
       <h2>Women's World Cup June-July 2019</h2>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
     </nav>
   );
 }
