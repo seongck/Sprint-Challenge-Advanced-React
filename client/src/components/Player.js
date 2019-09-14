384973185ba82578803b219f1b22b417c83b35eb
seongck <seongk@pm.me>
2019-09-13 20:30:54 -0700

    Add data-testid to player div

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Player({ name, country, searches }) {
+function Player({ key, name, country, searches }) {
   return (
-    <div className="player">
+    <div className="player" data-testid={key}>
       <h3>{name}</h3>
       <p>{country}</p>
       <p>{searches}</p>
